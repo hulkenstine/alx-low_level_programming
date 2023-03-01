1f82853f60e69e5549a64d5fe71161f2fcd3a755
hulkenstine <hulkenstine2018@gmail.com>
2023-03-01 02:19:59 -0800

    more functions and loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _ispper - checks for uppercase character
+ *
+ * @c: paramter to be checked
+ * Return: 1 if c is uppercasse and 0 otherwise
+ *
+ */
+
+int _isuppper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
