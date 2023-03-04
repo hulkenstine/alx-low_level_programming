b66ff64f45cebc19e853570bb5b8fe1a8d6ad4b8
hulkenstine <hulkenstine2018@gmail.com>
2023-03-03 23:38:52 -0800

    More functions and loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,10 +7,14 @@
  * Return: 1 if c is a digit otherwise 0
  */
 
-int _isdigit(int c);
+int _isdigit(int c)
 {
-	if (c <= '1' && c >= '9')
+	if (c >= '0' && c <= '9')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
